12d603f02e457ac078d970f19ec784bb470ace89
marty <techno.mooney@gmail.com>
2023-01-12 18:40:49 -0600

    Finished the instruction videos. now i will work on the project 1 for a bit

--- scratch.py
+++ scratch.py
@@ -1,4 +1,5 @@
 import random
+from dataclasses import dataclass
 
 class Dice:
     def __init__(self, sides=6):
@@ -6,6 +7,19 @@ class Dice:
 
     def roll(self):
         return random.randint(1,self.sides)
+
+
+@dataclass
+class StudentData:
+    name: str
+    school_id:str
+    gpa:float
+
+def __str__(self) -> str:
+    string_output = f"""Student Name: {self.name}
+        ID: {self.school_id}
+        Grade Point Average: {self.gpa}"""
+    return string_output
 
 
 class Student:
@@ -16,8 +30,23 @@ class Student:
 
     def __str__(self) -> str:
         string_output = f"""Student Name: {self.name}
-ID: {self.school_id}
-Grade Point Average: {self.gpa}"""
+    ID: {self.school_id}
+    Grade Point Average: {self.gpa}"""
         return string_output
 
+class Author:
+    def __init__(self,name) -> None:
+        self.name = name
+        self.published_books = []
+
+    def publish(self,title_name):
+        self.published_books.append(title_name.title())
     
+    def __str__(self) -> str:
+        author_string =f"{self.name.title()} has the following books published: \n"
+        if self.published_books:
+            for item_number,book_name in enumerate(self.published_books):
+                author_string+=f"\tBook {item_number+1}: {book_name}\n"
+        else:
+            author_string+="No Books Published yet."
+        return author_string
--- sxratch_test.py
+++ sxratch_test.py
@@ -4,3 +4,15 @@ test_student = Student("Bob Saggit","AS2415GD",3.75)
 print(test_student)
 test_student.gpa = 4.0
 print(test_student)
+
+
+shakespeare = Author("William Shakespeare")
+shakespeare.publish("Hamlet")
+shakespeare.publish("The Comedy of Errors")
+
+print(shakespeare)
+
+test_student_dataclass = Student("John Smith","GE9722AT",1.75)
+print(test_student_dataclass)
+test_student_dataclass.gpa =2.0
+print(test_student_dataclass)
